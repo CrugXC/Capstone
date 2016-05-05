5d6d2b208b34434acf219bdc9c349cd013327609
CrugXC <JTRixie@student.naperville203.org>
2016-05-05 14:57:16 -0500

    Added Player class

---
+++ Player.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author Jay Rixie
+ * @version 5/5/16
+ */
+public class Player
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private int x;
+
+    /**
+     * Default constructor for objects of class Player
+     */
+    public Player()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+   
+
+}
